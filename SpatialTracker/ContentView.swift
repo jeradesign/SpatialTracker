3564b3f8ac45e76a9ca74adee9b84b2afc02563e
John Brewer <jbrewer@jera.com>
2025-10-20 20:52:16 -0700

    Initial Commit

---
+++ SpatialTracker/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  SpatialTracker
+//
+//  Created by John Brewer on 10/4/25.
+//
+
+import SwiftUI
+import RealityKit
+import GLTFKit2
+
+struct ContentView: View {
+
+    @State private var enlarge = false
+
+    var body: some View {
+        RealityView { content in
+            // Add the initial RealityKit content
+            let urlResource = URLResource(name: "ShinySphere.glb")
+            if let url = URL(resource: urlResource),
+               let scene = try? await GLTFRealityKitLoader.load(from: url) {
+                content.add(scene)
+            }
+        } update: { content in
+            // Update the RealityKit content when SwiftUI state changes
+            if let scene = content.entities.first {
+                let uniformScale: Float = enlarge ? 1.4 : 1.0
+                scene.transform.scale = [uniformScale, uniformScale, uniformScale]
+            }
+        }
+        .gesture(TapGesture().targetedToAnyEntity().onEnded { _ in
+            enlarge.toggle()
+        })
+        .toolbar {
+            ToolbarItemGroup(placement: .bottomOrnament) {
+                VStack (spacing: 12) {
+                    Button {
+                        enlarge.toggle()
+                    } label: {
+                        Text(enlarge ? "Reduce RealityView Content" : "Enlarge RealityView Content")
+                    }
+                    .animation(.none, value: 0)
+                    .fontWeight(.semibold)
+
+                    ToggleImmersiveSpaceButton()
+                }
+            }
+        }
+    }
+}
+
+#Preview(windowStyle: .volumetric) {
+    ContentView()
+        .environment(AppModel())
+}
---
+++ SpatialTracker/ImmersiveView.swift
@@ -0,0 +1,37 @@
+//
+//  ImmersiveView.swift
+//  SpatialTracker
+//
+//  Created by John Brewer on 10/4/25.
+//
+
+import SwiftUI
+import RealityKit
+import GLTFKit2
+
+struct ImmersiveView: View {
+
+    var body: some View {
+        RealityView { content in
+            // Add the initial RealityKit content
+            let urlResource = URLResource(name: "ShinySphere.glb")
+            if let url = URL(resource: urlResource),
+               let ball = try? await GLTFRealityKitLoader.load(from: url) {
+                let leftBall = ball
+                let rightBall = ball.clone(recursive: true)
+                leftBall.position = SIMD3<Float>(-0.5, 1.5, -1.5)
+                content.add(leftBall)
+                rightBall.position = SIMD3<Float>(0.5, 1.5, -1.5)
+                content.add(rightBall)
+
+                // Put skybox here.  See example in World project available at
+                // https://developer.apple.com/
+            }
+        }
+    }
+}
+
+#Preview(immersionStyle: .mixed) {
+    ImmersiveView()
+        .environment(AppModel())
+}
